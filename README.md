2559308efef7f15cfb9a2731566a3c5712073e9c
Ardhitya Wiedha Irawan <ardhityawiedhairawan@gmail.com>
2016-09-06 14:01:28 +0700

    Initial

---
+++ README.md
@@ -0,0 +1,16 @@
+Get Email Account on Android
+==============================
+
+How to get primary email account on Android, assumsed the first email is the primary account.
+Display all account email on spinner.
+
+
+Result
+--------------
+<img src="screenshot.png" width="200"/>
+
+
+
+More Question ?
+--------------
+ardhityawiedhairawan@gmail.com  -
---
+++ app/src/main/java/aiueo/web/id/getemailaccount/MainActivity.java
@@ -0,0 +1,98 @@
+package aiueo.web.id.getemailaccount;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    TextView primaryAccount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String emailId = getEmiailID(getApplicationContext());
+        
+        primaryAccount = (TextView) findViewById(R.id.txtPrimaryAccount);
+        primaryAccount.setText(emailId);
+
+        addEmailToSpinner(getApplicationContext());
+
+
+    }
+
+    private String getEmiailID(Context context) {
+        AccountManager accountManager = AccountManager.get(context);
+        Account account = getAccount(accountManager);
+        if (account == null) {
+            return null;
+        } else {
+            return account.name;
+        }
+    }
+
+    private static Account getAccount(AccountManager accountManager) {
+        Account[] accounts = accountManager.getAccountsByType("com.google");
+        Account account;
+        if (accounts.length > 0) {
+            account = accounts[0];
+        } else {
+            account = null;
+        }
+        return account;
+    }
+
+
+    public void addEmailToSpinner(Context context) {
+        AccountManager accountManager = AccountManager.get(context);
+        Account[] accounts = accountManager.getAccountsByType("com.google");
+        List<String> list = new ArrayList<String>();
+        for (Account account : accounts) {
+            list.add(account.name);
+        }
+
+        Spinner spEmail = (Spinner) findViewById(R.id.spinnerEmail);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spEmail.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
